01964e2243e8d7d74b11cacb11d4e2e581c52ad3
yay5a <yay5a@users.noreply.github.com>
2025-07-31 10:24:18 -0500

    not fully understanding implementing mdx; will revert back to md for time being

--- mdx-components.js
+++ mdx-components.js
@@ -1,11 +1,5 @@
-import './app/globals.css';
 const components = {};
 export function useMDXComponents() {
-    return (
-        <article className="prose max-w-none">
-        
-            {components}
-        </article>
-    );
+    return components;
 }
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 import createMDX from "@next/mdx";
+const nextConfig = {
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+};
 const withMDX = createMDX({
 
     extension: /\.(md|mdx)$/,
 }
 );
-const nextConfig = {
-    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-};
 
 export default withMDX(nextConfig);
