572376f10a1879365b58ef63c25159ea9d845d4a
yay5a <yay5a@users.noreply.github.com>
2025-07-30 22:55:21 -0500

    added second blog; trying to align projects page to be similar to home

--- app/projects/page.js
+++ app/projects/page.js
@@ -3,11 +3,19 @@ import Image from "next/image";
 
 export default function Projects() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="container mx-auto p-4 sm:p-8 md:grid-cols-[260px_1fr] gap-8 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                <div className="min-h-screen bg-background text-foreground">
-                    <section className="flex flex-col items-center sm:items-start gap-4">
-                        <div>
+                <div className="space-y-6">
+                    <p className="text-foreground/70 text-lg sm:text-xl max-w-2xl mb-8 tracking-[-.01em]">
+                        Here are some of the projects I&apos;ve worked on. Feel
+                        free to explore and reach out if you have any questions
+                        or feedback!
+                    </p>
+                    <section className="space-y-6 mt-8 md:mt-0 card p-6">
+                        <h3 className="text-4xl md:text-6xl font-bold text white">
+                            Projects
+                        </h3>
+                        <div className="space-y-4">
                             <Image
                                 className="w: auto h: auto"
                                 src="/466858.jpg"
@@ -15,12 +23,8 @@ export default function Projects() {
                                 width={100}
                                 height={100}
                             />
-                            <h1 className="text-4xl md:text-6xl font-bold text white">Projects</h1>
                         </div>
                     </section>
-                    <p className="text-foreground/70 text-lg sm:text-xl max-w-2xl mb-8 tracking-[-.01em]">
-                        Here are some of the projects I've worked on. Feel free to explore and reach out if you have any questions or feedback!
-                    </p>
                 </div>
             </main>
         </div>
