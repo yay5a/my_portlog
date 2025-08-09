34b279f09d47ad6057c483d57b01a5fdc9ae5f45
yay5a <yay5a@users.noreply.github.com>
2025-08-09 16:08:46 -0500

    added vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,57 +2,61 @@ import React from "react";
 import Nav from "@/components/Nav";
 import Particles from "@/components/Particles";
 import "./globals.css";
-import { SiGithub } from 'react-icons/si';
+import { SiGithub } from "react-icons/si";
 import { SiLinkedin } from "react-icons/si";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
-    title: "Yaysa's Portfolio-log",
-    description:
-        "Yaysa's Portfolio-log - A showcase of my work and skills as a developer.",
+	title: "Yaysa's Portfolio-log",
+	description:
+		"Yaysa's Portfolio-log - A showcase of my work and skills as a developer.",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body className="antialiased">
-                <Particles />
-                <Nav />
-                {children}
-                <footer>
-                    {/* Social Links Section */}
-                    <section className="py-8 text-center">
-                        <p className="mt-8 text-lg">You can also find me on these other platforms:</p>
-                        <ul className="list-disc font-mono mt-4 space-y-2 inline-block text-left">
-                            <li>
-                                <a
-                                    href="https://github.com/yay5a"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="hover:underline"
-                                >
-                                    <SiGithub />
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="https://www.linkedin.com/in/mohammed-bhimjee"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="hover:underline"
-                                >
-                                    <SiLinkedin />
-                                </a>
-                            </li>
-                        </ul>
-                    </section>
-                    <div className="container py-4 mx-auto text-center">
-                        <p className="text-sm text-foreground/70">
-                            © {new Date().getFullYear()} Yaysa&apos;s
-                            Portfolio-log. All rights reserved.
-                        </p>
-                    </div>
-                </footer>
-            </body>
-        </html>
-    );
+	return (
+		<html lang="en">
+			<body className="antialiased">
+				<Particles />
+				<Nav />
+				{children}
+				<Analytics />
+				<footer>
+					{/* Social Links Section */}
+					<section className="py-8 text-center">
+						<p className="mt-8 text-lg">
+							You can also find me on these other platforms:
+						</p>
+						<ul className="list-disc font-mono mt-4 space-y-2 inline-block text-left">
+							<li>
+								<a
+									href="https://github.com/yay5a"
+									target="_blank"
+									rel="noopener noreferrer"
+									className="hover:underline"
+								>
+									<SiGithub />
+								</a>
+							</li>
+							<li>
+								<a
+									href="https://www.linkedin.com/in/mohammed-bhimjee"
+									target="_blank"
+									rel="noopener noreferrer"
+									className="hover:underline"
+								>
+									<SiLinkedin />
+								</a>
+							</li>
+						</ul>
+					</section>
+					<div className="container py-4 mx-auto text-center">
+						<p className="text-sm text-foreground/70">
+							© {new Date().getFullYear()} Yaysa&apos;s Portfolio-log. All
+							rights reserved.
+						</p>
+					</div>
+				</footer>
+			</body>
+		</html>
+	);
 }
